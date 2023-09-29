be8410d8662e81982e4e28c6b3a7f6c71c7a66fb
Benson Mwangi <kimaniben@gmail.com>
2023-09-29 07:29:29 -0400

    Updates

--- script.js
+++ script.js
@@ -112,23 +112,28 @@ let message = height < 4 ? "You are short":
               "You are a bit taller";
 console.log(message);
 //Multiplication
-let num1=31;
-let num2=2;
-let multiply= num1*num2;
-console.log(multiply);
+//let num1=31;
+//let num2=2;
+//let multiply= num1*num2;
+//console.log(multiply);
 // Math.random
-let random = (1,100);
-function getRandomInt(min,max){
-    return Math.floor(Math.random() * (max-min+1)) + min;
-}
-let randomInt=getRandomInt(1,100);
-console.log(randomInt);
-// Remainder
-let num3 =22;
-let num4 =6;
-let mod = num3%num4;
-console.log (mod);
-//Max
-let max=20;
-console.log(Math.max(2,3,15,20));
+//let random = (1,100);
+//function getRandomInt(min,max){
+    //return Math.floor(Math.random() * (max-min+1)) + min;
 
+//let randomInt=getRandomInt(1,100);
+//console.log(randomInt);
+// Remainder
+//let num3 =22;
+//let num4 =6;
+//let mod = num3%num4;
+//console.log (mod);
+//Max
+//let max=20;
+//console.log(Math.max(2,3,15,20));
+
+//traditional fuction
+
+//arrow function
+const area = (length,width) =>length*width;
+console.log(area(4,8))
